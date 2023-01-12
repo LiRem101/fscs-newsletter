1df134b811db7e1828c9d762a8d6c5d245ea4c98
Lian Remme <lirem101@hhu.de>
2023-01-12 22:05:48 +0800

    Server Initiated

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -1,4 +1,14 @@
-(ns clj.core)
+(ns clj.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [reitit.ring :as ring]))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/api" {:get (fn [req]
+                       {:status 200
+                        :body "ok"})}]])))
 
 (defn -main []
-  (println "Hello World"))
+  (println "Server started")
+  (run-server app {:port 4000}))
