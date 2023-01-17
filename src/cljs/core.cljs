06fd7292e78293e104794232c2ec9a6bb9e7c398
Lian Remme <lirem101@hhu.de>
2023-01-17 21:09:59 +0800

    Optimized cljs.core for React 18

--- src/cljs/core.cljs
+++ src/cljs/core.cljs
@@ -2,10 +2,12 @@
   (:require
     [helix.core :refer [defnc $]]
     [helix.dom :as d]
-    ["react-dom" :as dom]))
+    ["react-dom/client" :as rdom]))
 
 
 (defnc app [] (d/div "Hello World"))
 
+(defonce root (rdom/createRoot (js/document.getElementById "app")))
+
 (defn ^:export init []
-  (dom/render ($ app) (js/document.getElementById "app")))
+  (.render root ($ app)))
