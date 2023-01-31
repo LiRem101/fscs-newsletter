012c11e928ceacbc5c871db7c8b1114166cc5600
Lian Remme <lirem101@hhu.de>
2023-01-31 18:18:26 +0100

    Added nac funct

--- src/cljs/core.cljs
+++ src/cljs/core.cljs
@@ -4,8 +4,12 @@
     [helix.dom :as d]
     ["react-dom/client" :as rdom]))
 
+(defnc nav []
+       (d/nav {:class '[py-4 shadow]}
+              (d/div {:class '[container]}
+                     (d/h2 {:class '[text-xl]} "Contact Book"))))
 
-(defnc app [] (d/div "Hello World"))
+(defnc app [] (d/div ($ nav)))
 
 (defonce root (rdom/createRoot (js/document.getElementById "app")))
 
