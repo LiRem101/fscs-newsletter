67d3bdd84a18eb865c4c17f93456ea0aa25454ba
Lian Remme <lirem101@hhu.de>
2023-02-28 17:06:40 +0100

    Added GET in nav

--- src/cljs/core.cljs
+++ src/cljs/core.cljs
@@ -1,7 +1,9 @@
 (ns core
   (:require
+    [ajax.core :refer [GET]]
     [helix.core :refer [defnc $]]
     [helix.dom :as d]
+    [helix.hooks :as hooks]
     ["react-dom/client" :as rdom]))
 
 (defnc nav []
@@ -9,9 +11,15 @@
               (d/div {:class '[container]}
                      (d/h2 {:class '[text-xl]} "Contact Book"))))
 
-(defnc app [] (d/div ($ nav)))
+(defnc app []
+       (hooks/use-effect
+         :once
+         (GET "http://localhost:4000/api"
+              {:handler (fn [response]
+                            (.log js/console response))}))
+       (d/div ($ nav)))
 
 (defonce root (rdom/createRoot (js/document.getElementById "app")))
 
-(defn ^:export init []
+(defn ^:export ^:dev/after-load init []
   (.render root ($ app)))
