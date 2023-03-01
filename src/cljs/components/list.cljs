eb2a3a62383e46644c79424798c303670bd269d8
Lian Remme <lirem101@hhu.de>
2023-03-01 09:55:37 +0100

    Refactored list-item

--- src/cljs/components/list.cljs
+++ src/cljs/components/list.cljs
@@ -1,8 +1,12 @@
 (ns components.list
-  (:require [helix.core :refer [defnc]]
+  (:require [helix.core :refer [defnc $]]
             [helix.dom :as d]))
+
+(defnc newsletter-list-item [{:keys [news]}]
+       (d/li (d/div (d/p (:headline news)))))
 
 (defnc newsletter-list [{:keys [newsletter]}]
        (d/div
          (d/ul
-           (map-indexed (fn [i news] (d/li {:key i} (:headline news))) (:newsletter newsletter)))))
+           (map-indexed (fn [i news] ($ newsletter-list-item {:key i :news news}))
+                        (:newsletter newsletter)))))
